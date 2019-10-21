8ad3272167745ab3ab4b0b6c8469d9de28cdf806
dave <yefriddavid@gmail.com>
2019-10-21 10:04:36 -0300

    caching and recovery connection

--- ssh_tunnel.go
+++ ssh_tunnel.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"net"
+	// "time"
 	"strconv"
 )
 
@@ -31,6 +32,7 @@ func (tunnel *SSHTunnel) Start() error {
 
 	tunnel.Local.Port = listener.Addr().(*net.TCPAddr).Port
 
+	go createSshConnection(tunnel)
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -38,31 +40,83 @@ func (tunnel *SSHTunnel) Start() error {
 		}
 
 		tunnel.logf("accepted connection")
+		tunnel.logf("local port %s", strconv.Itoa(conn.RemoteAddr().(*net.TCPAddr).Port))
 		go tunnel.forward(conn)
 	}
 }
 
-func (tunnel *SSHTunnel) forward(localConn net.Conn) {
-	var serverConn *ssh.Client
+var sshConnectionWorking bool = false
+var workingFlag chan bool = make(chan bool)
+// var done bool = false
+// var total int = 0
+var serverConn *ssh.Client
+
+	
+func createSshConnection(tunnel *SSHTunnel){
+
 	var serverErr interface{}
+	var retry int
+
+	if sshConnectionWorking == false {
+
+		sshConnectionWorking = true
+
+		// createSshConnection(tunnel)
+		for {
+
+			retry ++
+			// c, err := d.dialDNS(ctx, network, server) 
+			serverConn, serverErr = ssh.Dial("tcp", tunnel.Server.String(), tunnel.Config)
+			if serverErr != nil {
+				tunnel.logf("server dial error: %s", serverErr)
+			} else {
+				close(workingFlag)
+				retry = 0
+				workingFlag = make(chan bool)
+				sshConnectionWorking = false
+		// tunnel.logf("Loaded")
+				//time.Sleep(10 * time.Second)
+				break
+			}
+
+		}
+	} else if serverConn == nil {
+				
+			
+		tunnel.logf("Waiting for the bus!")
+		<-workingFlag
+		tunnel.logf("The bus has arrived!")
+
+	}
+
+				
+		tunnel.logf("Loaded finished")
+}
+func (tunnel *SSHTunnel) forward(localConn net.Conn) {
 
 	var retry int
-	for {
 
-		retry ++
-		serverConn, serverErr = ssh.Dial("tcp", tunnel.Server.String(), tunnel.Config)
-		if serverErr != nil {
-			tunnel.logf("server dial error: %s", serverErr)
-		} else {
-			break
-		}
+
+	/*if sshConnectionWorking == false {
+
+		sshConnectionWorking = true
+
+		createSshConnection(tunnel)
+	} else if serverConn == nil {
+				
+			
+		tunnel.logf("Waiting for the bus!")
+		<-workingFlag
+		tunnel.logf("The bus has arrived!")
+
 	}
+	// serverConn.Status()
 
 	if retry > 1 {
 		tunnel.logf("Retry server: %s", strconv.Itoa(retry))
-	}
+	}*/
 
-	tunnel.logf("connected to %s (1 of 2)\n", tunnel.Server.String())
+	// tunnel.logf("connected to %s (1 of 2)\n", tunnel.Server.String())
 
 	var remoteConn net.Conn
 	var remoteError interface{}
@@ -71,14 +125,18 @@ func (tunnel *SSHTunnel) forward(localConn net.Conn) {
 	for {
 		retry ++
 		remoteConn, remoteError = serverConn.Dial("tcp", tunnel.Remote.String())
+		// remoteConn, remoteError = serverConn.Dial("tcp", "127.0.0.1:3306")
 		if remoteError != nil {
+			if retry > 3 {
+				//serverConn = nil
+				createSshConnection(tunnel)
+			}
 			tunnel.logf("remote dial error: %s", remoteError)
 		}else{
 
 			break
 		}
 	}
-			
 	if retry > 1 {
 		tunnel.logf("Retry remote: %s", strconv.Itoa(retry))
 	}
@@ -130,4 +188,5 @@ func NewSSHTunnel(tunnel string, auth ssh.AuthMethod, destination string, localP
 
 	return sshTunnel
 }
+
 
